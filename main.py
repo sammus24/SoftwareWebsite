772c194b9d99f554ddf3fa1954af006e3043763a
sammus24 <sammus2024@gmail.com>
2023-10-30 16:41:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name)
 def index():
     return render_template('index.html')
 
-@app.route('/result', methods=['POST'])
+@app.route('/results', methods=['POST'])
 def search():
     zip_code = request.form['zip_code']
     provider = request.form['provider']
