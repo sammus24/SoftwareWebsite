0f79b95beb970c016e99cb3dbd8f6bb22903c515
sammus24 <sammus2024@gmail.com>
2023-11-01 12:34:16 -0400

    Update API.py

--- API.py
+++ API.py
@@ -15,6 +15,7 @@ if response.status_code == 200:
     data = response.json()
     results = data.get("results", [])  # Get the "results" array from the JSON data
     
+    doctors = []
     for result in results:
         first_name = result.get("basic", {}).get("first_name", "")  # Get doctor's first name
         last_name = result.get("basic", {}).get("last_name", "")  # Get doctor's last name
@@ -23,8 +24,16 @@ if response.status_code == 200:
         address_data = result.get("addresses", [{}])[0]
         address = ", ".join(filter(None, [address_data.get("address_1", ""), address_data.get("address_2", ""), address_data.get("city", ""), address_data.get("state", ""), address_data.get("postal_code", "")]))
         
-        # Print doctor's name and address
-        print("Doctor's Name:", first_name,last_name)
-        print("Address:", address)
+        doctor_info = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "address": address
+        }
+                
+        doctors.append(doctor_info)
 else:
     print("Request failed with status code:", response.status_code)
+
+for item in doctors:
+    print(item)
+
