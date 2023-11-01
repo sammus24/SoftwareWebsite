abde5e121eb3c831114e7ed5b7b33ee7f32d49e5
sammus24 <sammus2024@gmail.com>
2023-11-01 13:00:27 -0400

    updates

--- function.js
+++ function.js
@@ -1,6 +1,20 @@
-function results() { 
-    fetch('/SoftwareWebsite/results') 
-      .then(response => response.json()) 
-      .then(data => console.log(data)) 
-      .catch(error => console.error(error)); 
-  } 
+<script>
+    function results() {
+        const zipCode = document.getElementById('zip_code').value;
+        const provider = document.getElementById('provider').value;
+    
+    fetch('/SoftwareWebsite/results', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+      body: JSON.stringify({ zip_code: zipCode, provider: provider }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // Handle the API response data as needed
+      })
+      .catch(error => console.error(error));
+  }
+</script>
