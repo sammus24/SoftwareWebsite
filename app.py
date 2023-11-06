0b2e44574e88da2e99bf5f100692ecb1fe85d3ea
sammus24 <sammus2024@gmail.com>
2023-11-06 17:08:50 -0500

    changea

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+
 from API import search_healthcare_providers  # Import the function from your module
+
+
 
 # Function to display the search results page
 def display_search_results(zip_code, provider):
@@ -7,17 +10,22 @@ def display_search_results(zip_code, provider):
     st.title("Search Results")
 
     if doctors:
-        st.write("Search Results:")
-        st.table(doctors)
+        for doctor in doctors:
+            st.write("Doctor Name:", doctor["first_name"], doctor["last_name"])
+            st.write("Address:", doctor["address"])
+            
+             
     else:
         st.write("No results found.")
-    st.write("Back to [Search](#search)")
+    
 
 # Function to display the search form page
 
 st.title("Healthcare Provider Search")
 zip_code = st.text_input("Enter ZIP code:")
 provider = st.text_input("Enter Provider Type:")
+
+
 
 if st.button("Search"):
     display_search_results(zip_code, provider)
--- application.py
+++ application.py
@@ -1,23 +1,23 @@
 import streamlit as st
 
-# Title of the form
 st.title("Simple Form Example")
 
-# Text input for name
+
+    # Text input for name
 name = st.text_input("Enter your name")
 
-# Text input for email
+    # Text input for email
 email = st.text_input("Enter your email")
 
-# Text area for message
+    # Text area for message
 message = st.text_area("Enter your message")
 
-# Checkbox for agreeing to terms
+    # Checkbox for agreeing to terms
 agree = st.checkbox("I agree to the terms and conditions")
 
-# Button to submit the form
+    # Button to submit the form
 if st.button("Submit"):
-    # Check if all fields are filled
+        # Check if all fields are filled
     if name and email and message and agree:
         # Process the form data (you can add your own processing logic here)
         st.success(f"Form submitted successfully!\nName: {name}\nEmail: {email}\nMessage: {message}")
