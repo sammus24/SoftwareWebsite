016a6f15e9b2d57b06d72181d679a92caedef1b2
sammus24 <sammus2024@gmail.com>
2023-11-21 18:23:58 -0500

    changes

--- Main.py
+++ Main.py
@@ -1,17 +1,19 @@
 import streamlit as st
 from Nav import Navigation
 
-st.write(st.session_state)
-st.title("Main Page")
-if st.button('Navigate'):
-    st.session_state.page = 'navigation'
+def main_page():
+    st.title("Main Page")
+    if st.button('Navigate'):
+        st.session_state.page = 'navigation'
 
 # Initialize session_state variables
 if 'page' not in st.session_state:
     st.session_state.page = 'main'
-
+    
+if st.session_state.page == 'main':
+    main_page()
 
 # Render pages based on session_stat
 if st.session_state.page == 'navigation':
     Navigation()
-    st.write(st.session_state)
+    
